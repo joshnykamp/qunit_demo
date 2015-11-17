a2be172a95d2e284358526313d6ab196fe5ea4f5
joshnykamp <joshn@voltagead.com>
2015-11-17 08:44:50 -0700

    added updated tests for demo

--- myTests.js
+++ myTests.js
@@ -2,16 +2,26 @@ function isEven(val) {
     return val % 2 === 0
 }
 
-test('isEven()', function() {
-    ok(isEven(0), 'Zero is an even number');
-    ok(isEven(2), 'So is two');
-    ok(isEven(-4), 'So is negative four');
-    ok(!isEven(1), 'One is not an even number');
-    ok(!isEven(-7), 'Neither is negative seven');
+test('isEven()', function(assert) {
+    assert.ok(isEven(0), 'Zero is an even number');
+    assert.ok(isEven(2), 'So is two');
+    assert.ok(isEven(-4), 'So is negative four');
+    assert.ok(!isEven(1), 'One is not an even number');
+    assert.ok(!isEven(-7), 'Neither is negative seven');
     // Fails
     //ok(isEven(3), 'Three is an even number');
 });
+/**
+ * Equal uses == so object and type comparison will fail.
+ */
+test('assertions', function(assert) {
+    assert.equal(1, 1, 'One equals one');
+    //equal( {}, {}, 'fails, these are different objects');
+    //equal( {a: 1}, {a: 1} , 'fails');
+    //equal( [], [], 'fails, there are different arrays');
+    //equal( [1], [1], 'fails');
+});
 
-test('assertions', function() {
-    equal(1, 1, 'One equals one');
+test('Check for same type', function(assert) {
+    assert.deepEqual( {}, {}, 'passes, objects have the same content');
 });
