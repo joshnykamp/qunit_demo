4ff45bd0b2d86381db5ca4ae81dc8306295ab19b
joshnykamp <joshn@voltagead.com>
2015-11-17 08:33:41 -0700

    initial push for demo

---
+++ myTests.js
@@ -0,0 +1,17 @@
+function isEven(val) {
+    return val % 2 === 0
+}
+
+test('isEven()', function() {
+    ok(isEven(0), 'Zero is an even number');
+    ok(isEven(2), 'So is two');
+    ok(isEven(-4), 'So is negative four');
+    ok(!isEven(1), 'One is not an even number');
+    ok(!isEven(-7), 'Neither is negative seven');
+    // Fails
+    //ok(isEven(3), 'Three is an even number');
+});
+
+test('assertions', function() {
+    equal(1, 1, 'One equals one');
+});
